fb5d4705ca20aa0f8beeb4dd807f03220cd8df9f
ChoiceG <efechoice@gmail.com>
2025-03-14 19:07:08 +0100

    Added domain name to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings ={
   address: "smtp.mailgun.org",
   username: ENV["MAILGUN_SMTP_LOGIN"],
   password: ENV["MAILGUN_SMTP_PASSWORD"],
-  domain: "",
+  domain: "https://devconnect.onrender.com/",
   authentication: :plain
 }
 ActionMailer::Base.delivery_method = :smtp
