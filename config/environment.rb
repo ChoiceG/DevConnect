1b5efa34070db52a85eb3ecdd00262a568b3fb15
ChoiceG <efechoice@gmail.com>
2025-03-15 15:28:42 +0100

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   port: 587,
   address: "smtp.mailgun.org",
-  username: ENV["MAILGUN_SMTP_LOGIN"],
+  username: ENV["MAILGUN_SMTP_USERNAME"],
   password: ENV["MAILGUN_SMTP_PASSWORD"],
   domain: "devconnect-d0xo.onrender.com", # Correct domain (no http/https)
   authentication: :plain
