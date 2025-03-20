ea56a3e580c3da322d1529e402b260e6be62e2a2
ChoiceG <efechoice@gmail.com>
2025-03-20 20:59:19 +0100

    Updated for deploy to Render

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,26 @@
 class Profile < ActiveRecord::Base
-    belongs_to :user
-    has_one_attached :avatar do |attachable|
-      attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
+  belongs_to :user
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
+  # Validations for other fields
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :job_title, presence: true
+  validates :phone_number, presence: true
+  validates :contact_email, presence: true
+  validates :description, presence: true
+
+  # Custom validation for avatar format
+  validate :avatar_format
+
+  private
+
+  # Custom method to validate avatar file type
+  def avatar_format
+    if avatar.attached? && !avatar.content_type.in?(%w(image/jpeg image/png image/gif))
+      errors.add(:avatar, 'must be a JPEG, PNG, or GIF')
     end
-  
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :job_title, presence: true
-    validates :phone_number, presence: true
-    validates :contact_email, presence: true
-    validates :description, presence: true
+  end
 end
