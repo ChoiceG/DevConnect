28cc2e7a52911cc3ad55ed4abe18f78b147ec614
ChoiceG <efechoice@gmail.com>
2025-03-21 08:43:57 +0100

    Active storage

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,17 +11,50 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
   # POST to /users/:user_id/profile
+  # def create
+  #   # Ensure that we have the user who is filling out form
+  #   @user = User.find(params[:user_id])
+  #   # Create profile linked to this specific user
+  #   @profile = @user.build_profile(profile_params)
+  #   if @profile.save
+  #     flash[:success] = "Profile updated!"
+  #     redirect_to user_path(id: params[:user_id])
+  #   else
+  #     render action: :new
+  #   end
+  # end
+
+  # def create
+  #   @user = User.find(params[:user_id])
+  #   @profile = @user.build_profile(profile_params)
+  #   puts "Profile: #{@profile.inspect}"
+  #   if @profile.save
+  #     puts "Profile saved successfully!"
+  #     flash[:success] = "Profile updated!"
+  #     redirect_to user_path(id: params[:user_id])
+  #   else
+  #     puts "Profile errors: #{@profile.errors.full_messages.inspect}"
+  #     flash.now[:error] = @profile.errors.full_messages.join(", ")
+  #     render action: :new, status: :unprocessable_entity
+  #   end
+  # end
+
   def create
-    # Ensure that we have the user who is filling out form
     @user = User.find(params[:user_id])
-    # Create profile linked to this specific user
-    @profile = @user.build_profile(profile_params)
-    if @profile.save
-      flash[:success] = "Profile updated!"
-      redirect_to user_path(id: params[:user_id])
-    else
-      render action: :new
+    ActiveRecord::Base.transaction do
+      @profile = @user.build_profile(profile_params)
+      if @profile.save
+        flash[:success] = "Profile updated!"
+        redirect_to user_path(id: params[:user_id])
+      else
+        flash.now[:error] = @profile.errors.full_messages.join(", ")
+        render action: :new, status: :unprocessable_entity
+        raise ActiveRecord::Rollback # Ensure rollback on validation failure.
+      end
     end
+  rescue => e
+    Rails.logger.error "Profile creation error: #{e.message}\n#{e.backtrace.join("\n")}"
+    render :new, status: :internal_server_error
   end
 
   # GET request to /users/:user_id/profile/edit
