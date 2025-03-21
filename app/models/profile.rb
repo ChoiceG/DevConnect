9c5b8db6d8d7e9085e4a0633224e64e3b7610628
ChoiceG <efechoice@gmail.com>
2025-03-21 08:22:23 +0100

    Edited for aws

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,8 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  has_one_attached :avatar 
-  
-  # do |attachable|
-    # attachable.variant :thumb, resize_to_limit: [100, 100]
-  # end
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 
   # Validations for other fields
   validates :first_name, presence: true
@@ -13,17 +11,16 @@ class Profile < ActiveRecord::Base
   validates :phone_number, presence: true
   validates :contact_email, presence: true
   validates :description, presence: true
-  validates :avatar, presence: true
 
   # Custom validation for avatar format
-  # validate :avatar_format
+  validate :avatar_format
 
-  # private
+  private
 
-  # # Custom method to validate avatar file type
-  # def avatar_format
-  #   if avatar.attached? && !avatar.content_type.in?(%w(image/jpeg image/png image/gif))
-  #     errors.add(:avatar, 'must be a JPEG, PNG, or GIF')
-  #   end
-  # end
+  # Custom method to validate avatar file type
+  def avatar_format
+    if avatar.attached? && !avatar.content_type.in?(%w(image/jpeg image/png image/gif))
+      errors.add(:avatar, 'must be a JPEG, PNG, or GIF')
+    end
+  end
 end
